79818396465d6a1928078f1a276e6e912512b125
HenriqueSOliver <HenriqueSOliver85@gmail.com>
2021-07-12 20:00:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,5 @@
 # Our main file > (Arquivo principal)
 
-'''
-import speech_recognition as sr
-
-#creating recognizer > (Criar um reconhecedor)
-r = sr.Recognizer()
-
-#open audio device > (Abrir o microfone)
-with sr.Microphone() as source:
-    while True:
-        audio = r.listen(source) # Define microfone como fonte de áudio
-
-        print(r.recognizer_google(audio,language='pt'))
-
-'''
-
 #!/usr/bin/env python3
 
 from vosk import Model, KaldiRecognizer
