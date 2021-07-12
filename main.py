14154d636503c0e5fbb698df2b55d00c66832c56
HenriqueSOliver <HenriqueSOliver85@gmail.com>
2021-07-12 18:35:44 -0300

    Recognition Definitions
    Criar reconhecedor e habilitar microfone

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-# Our main file> (Arquivo principal)
+# Our main file > (Arquivo principal)
+
+import speech_recognition as sr
+
+#creating recognizer > (Criar um reconhecedor)
+r = sr.recognizer()
+
+#open audio device > (Abrir o microfone)
+with sr.Microphone() as source:
+    audio = r.listen(source) # Define microfone como fonte de áudio
+
+    print(r.recognizer_google(audio))
